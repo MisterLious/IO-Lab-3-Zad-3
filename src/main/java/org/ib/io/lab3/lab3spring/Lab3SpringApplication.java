da345d71a51a345dca91d4e48f540233664399c1
Maciej Łochowski <249969@student.pwr.edu.pl>
2021-03-29 08:04:19 +0200

    Initial Commit

---
+++ src/main/java/org/ib/io/lab3/lab3spring/Lab3SpringApplication.java
@@ -0,0 +1,14 @@
+package org.ib.io.lab3.lab3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3SpringApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/org/ib/io/lab3/lab3spring/Lab3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ib.io.lab3.lab3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
